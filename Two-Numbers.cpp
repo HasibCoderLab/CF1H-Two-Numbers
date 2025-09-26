90c8fb3b675ae2bd97b60d908422f6092871ad33
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-26 16:21:08 +0600

    reupdate

--- Two-Numbers.cpp
+++ Two-Numbers.cpp
@@ -14,8 +14,8 @@ int main()
     double A, B;
     cin >> A >> B;
     cout << "floor " << A << " / " << B << " = " << floor(A / B) << endl;
-    cout << "ceil " << A << " / " << B << " = " << floor(A / B) << endl;
-    cout << "round " << A << " / " << B << " = " << floor(A / B) << endl;
+    cout << "ceil " << A << " / " << B << " = " << ceil(A / B) << endl;
+    cout << "round " << A << " / " << B << " = " << round(A / B) << endl;
 
     return 0;
 }
